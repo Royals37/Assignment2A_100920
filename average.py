a3d4a1fa081680543348ddcd5a96b5b69c736ab0
Royals37 <72286684+Royals37@users.noreply.github.com>
2020-10-09 18:51:01 -0400

    Assignment 2a
    Assignment 2a

---
+++ average.py
@@ -0,0 +1,19 @@
+# Author: Royce Stevens
+# Date 10/7/2020
+# The script will ask the user to input five numbers
+# then the output will be average of the five numbers
+
+print("Please enter five numbers.")
+
+num1 = float(input())
+num2 = float(input())
+num3 = float(input())
+num4 = float(input())
+num5 = float(input())
+
+s = num1 + num2 + num3 + num4 + num5
+
+S1 = s/5
+
+print("The average of those numbers is: ")
+print(S1)
